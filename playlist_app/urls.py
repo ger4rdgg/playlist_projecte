813b9358cc3843a2c8896597f700d98a277ac18d
Binari Software <rmunne@e-binari.com>
2020-05-10 10:00:18 +0200

    varis entorn

--- playlist_app/urls.py
+++ playlist_app/urls.py
@@ -5,10 +5,10 @@ from users import views
 from .views import login, index
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
     url(r'^users/', include('users.urls')),
     url(r'^$', index),
     path('', include('social_django.urls', namespace='social')),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout')
+    
 
 ]
--- playlist_projecte/settings.py
+++ playlist_projecte/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django',
+    'behave_django',
     'playlist_app'
 
 ]
